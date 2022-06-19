a1287add87319f734e9467caeacdc4b5bdab9a75
Luben Gonsalves <lubenfrank48@gmail.com>
2022-06-19 10:06:37 +0200

    Two sum

--- 1.two-sum.js
+++ 1.two-sum.js
@@ -10,8 +10,15 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    
+var twoSum = function (nums, target) {
+  let hashMap = {};
+  for (let i = 0; i < nums.length; i++) {
+    let requiredNum = target - nums[i];
+    if (requiredNum in hashMap) {
+      return [hashMap[requiredNum], i];
+    } else {
+      hashMap[nums[i]] = i;
+    }
+  }
 };
 // @lc code=end
-
