09b129b9f237c9deb2b126e9e7e782f03e72f27e
Luben Gonsalves <lubenfrank48@gmail.com>
2022-06-22 20:01:26 +0200

    best time to buy and sell stock

---
+++ 121.best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=121 lang=javascript
+ *
+ * [121] Best Time to Buy and Sell Stock
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {
+  let maxProfit = 0;
+  let minSoFar = Number.MAX_VALUE;
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minSoFar) {
+      minSoFar = prices[i];
+    } else {
+      maxProfit = Math.max(maxProfit, prices[i] - minSoFar);
+    }
+  }
+
+  return maxProfit;
+};
+// @lc code=end
