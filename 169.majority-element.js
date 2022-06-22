171466132bb5a05b85507191f54574bf03f91a73
Luben Gonsalves <lubenfrank48@gmail.com>
2022-06-22 22:56:37 +0200

    majority element

---
+++ 169.majority-element.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=169 lang=javascript
+ *
+ * [169] Majority Element
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  let hashMap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] in hashMap) {
+      hashMap[nums[i]] = hashMap[nums[i]]++;
+    } else {
+      hashMap[nums[i]] = 1;
+    }
+  }
+
+  let max = 0;
+  for (let num in hashMap) {
+    let potentialMax = num / hashMap[num];
+    if (potentialMax > max) {
+      max = potentialMax;
+    }
+  }
+  return max;
+};
+// @lc code=end
