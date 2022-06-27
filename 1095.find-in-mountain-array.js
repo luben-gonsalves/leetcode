6a2f699f6363b7a47b8018412647e2d3144f83f8
Luben Gonsalves <lubenfrank48@gmail.com>
2022-06-27 17:24:06 +0200

    search in a 2d matrix

---
+++ 1095.find-in-mountain-array.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=1095 lang=javascript
+ *
+ * [1095] Find in Mountain Array
+ */
+
+// @lc code=start
+/**
+ * // This is the MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * function MountainArray() {
+ *     @param {number} index
+ *     @return {number}
+ *     this.get = function(index) {
+ *         ...
+ *     };
+ *
+ *     @return {number}
+ *     this.length = function() {
+ *         ...
+ *     };
+ * };
+ */
+
+/**
+ * @param {number} target
+ * @param {MountainArray} mountainArr
+ * @return {number}
+ */
+var findInMountainArray = function (target, nums) {};
+// @lc code=end
---
+++ 852.peak-index-in-a-mountain-array.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=852 lang=javascript
+ *
+ * [852] Peak Index in a Mountain Array
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function (nums) {};
+// @lc code=end
