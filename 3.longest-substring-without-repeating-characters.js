39074e454c874a65b68ae0403f3b8221392b697a
luben <lubenfrank48@gmail.com>
2022-07-03 13:30:29 +0200

    longest substring without repeating characters

---
+++ 3.longest-substring-without-repeating-characters.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=3 lang=javascript
+ *
+ * [3] Longest Substring Without Repeating Characters
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+    let i = 0, j = 0;
+    let max = Number.MIN_VALUE;
+    let hashMap = {};
+
+    while(j < s.length) {
+        if(hashMap[s[j]] >= i) {
+            i = hashMap[s[j]] +1
+        }
+        hashMap[s[j]] = j;
+        max = Math.max(max, j - i + 1)
+        j++;
+    }
+    return max;
+};
+
+// @lc code=end
+
