dea47a6c6e2d551244335c118869f1436da1fda5
Luben Gonsalves <lubenfrank48@gmail.com>
2022-07-17 17:13:38 +0200

    is valid bst

---
+++ 98.validate-binary-search-tree.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=98 lang=javascript
+ *
+ * [98] Validate Binary Search Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (
+  root,
+  min = Number.MIN_SAFE_INTEGER,
+  max = Number.MAX_SAFE_INTEGER
+) {
+  if (!root) return true;
+  if (root.val <= min || root.val >= max) return false;
+
+  return (
+    isValidBST(root.left, min, root.val) &&
+    isValidBST(root.right, root.val, max)
+  );
+};
+
+// @lc code=end
