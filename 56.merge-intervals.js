841d21c612fee8b56c4768632e594d6bc92e8137
Luben Gonsalves <lubenfrank48@gmail.com>
2022-07-20 21:51:49 +0200

    merge intervals

---
+++ 56.merge-intervals.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=56 lang=javascript
+ *
+ * [56] Merge Intervals
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function (intervals) {
+  if (!intervals.length) return [];
+  intervals.sort((a, b) => a[0] - b[0]);
+
+  const result = [intervals[0]];
+
+  for (let [start, end] of intervals) {
+    const endPrev = result.at(-1)[1];
+    if (start <= endPrev) result.at(-1)[1] = Math.max(end, endPrev);
+    else result.push([start, end]);
+  }
+  return result;
+};
+// @lc code=end
