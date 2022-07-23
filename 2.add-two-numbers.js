3d5f64341973fcd0e5a9d2822e06820c365c476b
Luben Gonsalves <lubenfrank48@gmail.com>
2022-07-22 17:22:20 -0700

    minor changes

--- 2.add-two-numbers.js
+++ 2.add-two-numbers.js
@@ -22,6 +22,7 @@ var addTwoNumbers = function (l1, l2) {
   let resultNode = new ListNode(0);
   let returnNode = resultNode;
 
+  //l1 && l2
   while (l1 || l2) {
     const sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + carry;
     resultNode.next = new ListNode(sum >= 10 ? sum % 10 : sum);
