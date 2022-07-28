893b0a1b2a40f6ed64d04c5ad98d3e509c96951d
Luben Gonsalves <lubenfrank48@gmail.com>
2022-07-28 22:42:05 +0200

    roman to integer

---
+++ 13.roman-to-integer.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=13 lang=javascript
+ *
+ * [13] Roman to Integer
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  const legend = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (legend[s[i]] < legend[s[i + 1]]) {
+      total += legend[s[i + 1]] - legend[s[i]];
+      i++;
+    } else {
+      total += legend[s[i]];
+    }
+  }
+
+  return total;
+};
+// @lc code=end
