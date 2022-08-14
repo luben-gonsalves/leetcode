6790011c8a342bfd3b6abcdfbf735b53b6d2f8ec
Luben Gonsalves <lubenfrank48@gmail.com>
2022-08-13 22:35:17 -1200

    valid pallindrome

---
+++ 125.valid-palindrome.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=125 lang=javascript
+ *
+ * [125] Valid Palindrome
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.toLowerCase().replace(/[^a-z0-9]/gi,'');
+    for (let i = 0, j = s.length - 1; i <= j; i++, j--) {
+      if (s.charAt(i) !== s.charAt(j)) return false;
+    }
+    return true;
+  };
+// @lc code=end
+
